a59b6d1948ebdd746141d8e690bebcfaf09e3e3f
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2025-03-29 14:34:36 +0700

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,29 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+    },
+    plugins: {
+      js,
+      "@typescript-eslint": tseslint,
+      react: pluginReact,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Tidak perlu React di scope
+      "react/jsx-uses-react": "off", // Tidak perlu React untuk JSX
+      "no-undef": "off", // Untuk JSX otomatis
+    },
+  },
 ]);
