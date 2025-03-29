d472784d12658fc5be8cdf079bbb75429327f535
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2025-03-29 14:38:18 +0700

    fix eslint 2

--- src/ui/landing/navbar/nav-link.tsx
+++ src/ui/landing/navbar/nav-link.tsx
@@ -1,21 +1,10 @@
 'use client';
 
-import {
-  UserGroupIcon,
-  Bars3Icon,
-} from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
 import Search from '../../search';
 import DropdownMenu from './dropdownMenu';
 
-const links = [
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
-
 export default function NavLinks() {
-  const pathname = usePathname();
 
   return (
     <div className="flex items-center gap-4 w-full">
