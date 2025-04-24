0fbbe6183b92572789de482ead1ed986422811a5
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2025-04-24 11:41:02 +0700

    comment middlewate.js

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,25 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+// // import { NextResponse } from "next/server";
+// // import type { NextRequest } from "next/server";
+// // import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: NextRequest) {
+// export async function middleware(req: NextRequest) {
 
-  // const token = await getToken({
-  //   req,
-  //   secret: process.env.NEXTAUTH_SECRET,
-  // });
+//   // const token = await getToken({
+//   //   req,
+//   //   secret: process.env.NEXTAUTH_SECRET,
+//   // });
 
-  // if (!token && req.nextUrl.pathname.startsWith("/admin")) {
-  //   const loginUrl = new URL("/login", req.url);
-  //   loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
-  //   return NextResponse.redirect(loginUrl);
-  // }
+//   // if (!token && req.nextUrl.pathname.startsWith("/admin")) {
+//   //   const loginUrl = new URL("/login", req.url);
+//   //   loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
+//   //   return NextResponse.redirect(loginUrl);
+//   // }
 
-  // return NextResponse.next();
-}
+//   // return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/admin/:path*"],
-};
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// };
+
+export async function middleware() {}
